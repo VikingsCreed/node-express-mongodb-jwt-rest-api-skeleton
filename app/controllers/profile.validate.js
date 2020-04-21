e3e5868c9397d4e9ed3940cab2f25cbac422673a
Erlend Ellefsen <ekbe_97@hotmail.no>
2020-04-21 17:17:31 +0200

    save user fix

--- app/controllers/profile.validate.js
+++ app/controllers/profile.validate.js
@@ -1,5 +1,4 @@
 const { validationResult } = require('../middleware/utils')
-const validator = require('validator')
 const { check } = require('express-validator')
 
 /**
@@ -18,20 +17,6 @@ exports.updateProfile = [
     .not()
     .isEmpty()
     .withMessage('IS_EMPTY'),
-  check('phone')
-    .exists()
-    .withMessage('MISSING')
-    .not()
-    .isEmpty()
-    .withMessage('IS_EMPTY')
-    .trim(),
-  check('city')
-    .exists()
-    .withMessage('MISSING')
-    .not()
-    .isEmpty()
-    .withMessage('IS_EMPTY')
-    .trim(),
   check('country')
     .exists()
     .withMessage('MISSING')
@@ -45,14 +30,6 @@ exports.updateProfile = [
     .not()
     .isEmpty()
     .withMessage('IS_EMPTY'),
-  check('urlTwitter')
-    .optional()
-    .custom((v) => (v === '' ? true : validator.isURL(v)))
-    .withMessage('NOT_A_VALID_URL'),
-  check('urlGitHub')
-    .optional()
-    .custom((v) => (v === '' ? true : validator.isURL(v)))
-    .withMessage('NOT_A_VALID_URL'),
   (req, res, next) => {
     validationResult(req, res, next)
   }
--- app/controllers/users.validate.js
+++ app/controllers/users.validate.js
@@ -1,5 +1,4 @@
 const { validationResult } = require('../middleware/utils')
-const validator = require('validator')
 const { check } = require('express-validator')
 
 /**
@@ -51,14 +50,6 @@ exports.createItem = [
     .isLength({
       min: 17
     }),
-  check('urlTwitter')
-    .optional()
-    .custom((v) => (v === '' ? true : validator.isURL(v)))
-    .withMessage('NOT_A_VALID_URL'),
-  check('urlGitHub')
-    .optional()
-    .custom((v) => (v === '' ? true : validator.isURL(v)))
-    .withMessage('NOT_A_VALID_URL'),
   (req, res, next) => {
     validationResult(req, res, next)
   }
@@ -108,28 +99,6 @@ exports.updateItem = [
     .not()
     .isEmpty()
     .withMessage('IS_EMPTY'),
-  check('phone')
-    .exists()
-    .withMessage('MISSING')
-    .not()
-    .isEmpty()
-    .withMessage('IS_EMPTY')
-    .trim(),
-  check('city')
-    .exists()
-    .withMessage('MISSING')
-    .not()
-    .isEmpty()
-    .withMessage('IS_EMPTY')
-    .trim(),
-  check('urlTwitter')
-    .optional()
-    .custom((v) => (v === '' ? true : validator.isURL(v)))
-    .withMessage('NOT_A_VALID_URL'),
-  check('urlGitHub')
-    .optional()
-    .custom((v) => (v === '' ? true : validator.isURL(v)))
-    .withMessage('NOT_A_VALID_URL'),
   check('id')
     .exists()
     .withMessage('MISSING')
